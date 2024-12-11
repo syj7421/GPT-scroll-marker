62bb694b0d02dbf6eb6b80d4d45e693a298958c2
Sooyoung Jung <91307997+syj7421@users.noreply.github.com>
2024-12-11 15:53:25 +1100

    jhbjhbg

---
+++ popup.js
@@ -0,0 +1,6 @@
+const hideTheWidgetBtn = document.getElementById("hide-the-widget-btn");
+const deleteMarkers = document.getElementById("delete-markers-btn");
+
+hideTheWidgetBtn.addEventListener("click", () => {
+    console.log("clicked!!");
+});
