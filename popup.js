451b157fd8ed73dcecb16bf6777886fdf7cd8ac1
Sooyoung Jung <91307997+syj7421@users.noreply.github.com>
2024-12-18 16:07:43 +1100

    popup

--- popup.js
+++ popup.js
@@ -1,19 +1,23 @@
 const hideTheWidgetBtn = document.getElementById("hide-the-widget-btn");
 let isHidden = false;
+
 hideTheWidgetBtn.addEventListener("click", () => {
-    if (!isHidden){
+    if (!isHidden) {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             chrome.tabs.sendMessage(tabs[0].id, { action: "hide_widget" });
         });
         hideTheWidgetBtn.textContent = "Show the Widget";
-
-    }
-    else{
+    } else {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             chrome.tabs.sendMessage(tabs[0].id, { action: "show_widget" });
         });
         hideTheWidgetBtn.textContent = "Hide the Widget";
     }
     isHidden = !isHidden;
+});
 
+const colorPicker = document.getElementById("color-picker");
+colorPicker.addEventListener("change", (event) => {
+    const selectedColor = event.target.value;
+    hideTheWidgetBtn.style.backgroundColor = selectedColor;
 });
